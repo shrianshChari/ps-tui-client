c8590ae1bc5e36ce6b9d09b20befab25da788858
Shriansh Chari <30420527+shrianshChari@users.noreply.github.com>
2026-04-05 01:04:59 -0700

    Fix source to algorithm of username colors

--- utils/username_color.go
+++ utils/username_color.go
@@ -10,7 +10,7 @@ var ColorCache map[string]string = make(map[string]string)
 
 // Converts a PS username into a color in RGB
 // Original algorithm source:
-// https://github.com/shrianshChari/pokemon-showdown-client/blob/master/play.pokemonshowdown.com/src/battle-log.ts#L562
+// https://github.com/smogon/pokemon-showdown-client/blob/master/play.pokemonshowdown.com/src/battle-log.ts#L1166
 func UsernameToColor(username string) string {
 	id := ToID(username)
 
